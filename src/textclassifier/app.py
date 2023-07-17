3356376d08c11f4e236b60293c80333b651311ca
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-18 00:00:56 +0530

    added overall sentiment

--- src/textclassifier/app.py
+++ src/textclassifier/app.py
@@ -1,27 +1,29 @@
 from flask import Flask, request
-from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
+from transformers import RobertaTokenizerFast, \
+    TFRobertaForSequenceClassification, pipeline
 
 
 app = Flask(__name__)
 tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
 model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
 emotion_classifier = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
-    
+sentiment_classifier = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+
+
+sentiments = ("negative", "positive")
+
 
 @app.post('/classify')
 def classify_text():
     body = request.json
-    data = [row['text'] for row in  body['inputs']]
-    emotion_labels = emotion_classifier(data)
+    data = [row['text'] for row in body['conversation']]
+
     return {
-        'data': emotion_labels,
-        'status': 'success'
+        'emotions': emotion_classifier(data),
+        'overall_sentiment': sentiment_classifier([
+            body['transcript']
+        ])[0]
     }
-
-
-@app.get('/test/<int:value>')
-def test_endpoint(value):
-    return str(value)
 
 
 if __name__ == '__main__':
